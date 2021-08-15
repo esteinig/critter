039574037d057f241f8ef5d3ff122acd4aa497d4
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-08-15 18:27:27 +1000

    xml string and kwarg long format; all tests pass

--- critter/blocks/clocks.py
+++ critter/blocks/clocks.py
@@ -51,6 +51,12 @@ class Clock(BaseModel):
     def xml_branch_rate_model(self) -> str:
         return ''
 
+    @validator('prior', pre=True)
+    def check_if_prior_is_list(cls, field):
+        if isinstance(field, Prior):
+            return [Prior]
+        return field
+
     @validator('prior')
     def prior_must_be_clock_prior(cls, field):
         for prior in field:
@@ -59,7 +65,7 @@ class Clock(BaseModel):
             ):
                 raise ValidationError(
                     "Clocks must be populated with valid clock prior instances of: "
-                    "ClockRate, UCREPrior, UCRLMeanPrior or UCRLSDPrior"
+                    "ClockRatePrior, UCREPrior, UCRLMeanPrior or UCRLSDPrior"
                 )
         return field
 
